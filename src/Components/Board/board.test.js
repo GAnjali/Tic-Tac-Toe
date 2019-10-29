aa822624ecb88b7e824983213ca2b6cf702b0dea
ganjali <gaddam.anjali@oracle.com>
2019-10-29 11:16:39 +0530

    add onClick function testing by passing mock function

--- src/Components/Board/board.test.js
+++ src/Components/Board/board.test.js
@@ -6,3 +6,11 @@ it('Board component renders without crashing', () => {
     let squares = Array(9).fill(1);
     shallow(<Board squares={squares}/>);
 });
+
+it('Should call onClick by clicking on Board square', () => {
+   let squares = Array(9).fill(0);
+   let onClick = jest.fn();
+   let wrapper = mount(<Board squares={squares} onClick={onClick}/>);
+   wrapper.find('button.square').first().simulate('click');
+   expect(onClick).toBeCalledWith(0);
+});
--- src/Components/Square/square.test.js
+++ src/Components/Square/square.test.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import Square from './square'
-import {shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
+import Board from "../Board/board";
 
 it('Square component renders without crashing', function () {
     let squares = Array(9).fill(null);
     shallow(<Square/>);
+});
+
+it('Should call onClick by clicking on Square square', () => {
+    let squares = Array(9).fill(0);
+    let onClick = jest.fn();
+    let wrapper = mount(<Square squares={squares} onClick={onClick}/>);
+    wrapper.find('button.square').first().simulate('click');
+    expect(onClick).toHaveBeenCalledTimes(1);
 });
